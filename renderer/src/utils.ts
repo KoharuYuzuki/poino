10f062f35fc13ab0931b6404e6d7653f31b8b354
KoharuYuzuki <79073521+KoharuYuzuki@users.noreply.github.com>
2023-02-02 12:23:18 +0900

    小数点が2つ以上含まれないように修正

--- renderer/src/utils.ts
+++ renderer/src/utils.ts
@@ -1,4 +1,17 @@
 export function extractNumbers(str: string) {
   const numStr = [...[...new Array(10)].map((_, i) => String(i)), '.']
-  return [...str].filter((x) => numStr.includes(x)).join('')
+  let existedDot = false
+  return (
+    [...str]
+    .filter((x) => numStr.includes(x))
+    .reduce((joined, x) => {
+      if (x !== '.') return joined + x
+      if (!existedDot) {
+        existedDot = true
+        return joined + x
+      } else {
+        return joined
+      }
+    }, '')
+  )
 }
